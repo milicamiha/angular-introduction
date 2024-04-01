b859b27e460689bb3715073bda6008fccb7e0f5b
Mihajlovic Milica <mihajlovicmilica@yahoo.com>
2024-04-01 22:02:23 +0300

    component input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,20 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 })
 export class AppComponent {
   name = 'Militsa';
+
+  person0= {
+    givenName: 'Milica',
+    surName: 'Mihajlovic',
+    age: 37,
+    email: 'megamilica@gmail.com',
+    address: 'Athens, Greece'
+  }
+
+  person1 = {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 40,
+    email: 'johndoe@gmail.com',
+    address: 'New York, USA'
+  };
 }
